c9ac7b76f34a7d3f2401f724270f4b88f7232472
Nikhil <nikhilnaik6489@gmail.com>
2025-02-03 02:24:53 +0530

    Third Point

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-/* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
+
 import bodyParser from "body-parser";
 import fs from "fs";
 import qr from "qr-image";
 import express from "express";
+import path from "path";
 var qr_data="https://google.com";
 const port=3000;
 const app=express();
-var qr_svg = qr.image(qr_data);
-qr_svg.pipe(fs.createWriteStream('public/qr_img.png'));
-app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, "public")));
+var qr_svg;
+qr_svg.pipe(fs.createWriteStream('public/qr_img.png'));
+
+
 app.get("/",(req,res)=>{
   res.render("index.ejs");
 })
